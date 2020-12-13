5d1a6e2c6d41c8086e52ac293f0b679270e71e20
codehia <ach4825@gmail.com>
2020-12-13 21:30:15 +0530

    add endpoints to add and fetch data

---
+++ main.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from tree import Tree
+
+app = FastAPI()
+
+
+class Dim(BaseModel):
+    key: str
+    val: str
+
+
+class Metrics(BaseModel):
+    key: str
+    val: int
+
+
+class Item(BaseModel):
+    dim: List[Dim] = None
+    metrics: List[Metrics] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/v1/insert/")
+def inset(item: Item):
+    tree = Tree()
+    return tree.insert_data(item.dict())
+
+
+@app.get("/v1/query/{key}/{value}")
+def query(key: str, value: str):
+    tree = Tree()
+    return tree.fetch_data(key, value)
+    return {"Hello": "World"}
+
--- tree.py
+++ tree.py
@@ -1,80 +1,90 @@
+from fastapi import status
+
+
 class Node(object):
-
-    """Docstring for Node. """
-
     def __init__(self, value, metrics={"webrq": 0, "timespent": 0}, children=[]):
-        """
-        :Docstring for Node.: TODO
-
-        """
         self.dim_type = value.get("key")
         self.dim_value = value.get("value")
         self.metrics = metrics
         self.children = children
 
     def __str__(self) -> str:
-        chil = [ch.dim_value for ch in self.children]
-        return f"{self.dim_type} {self.dim_value} {str(self.metrics)} {chil}"
+        children = ",".join([ch.dim_value for ch in self.children])
+        return f"{self.dim_type} {self.dim_value} {str(self.metrics)} {children}"
 
 
-def create_base_tree(root):
-    country_level = [[{"key": "country", "value": "IN"}, {"webreq": 0, "timespent": 0}]]
-    root.children = [Node(value, metrics) for value, metrics in country_level]
-    device_level = [
-        [{"key": "device", "value": "mobile"}, {"webreq": 0, "timespent": 0}],
-        [{"key": "device", "value": "web"}, {"webreq": 0, "timespent": 0}],
-    ]
-    for country in root.children:
-        children = []
-        for value, metrics in device_level:
-            children.append(Node(value, metrics))
-        country.children = children
+class Tree(object):
+    def __init__(self):
+        self.root = self._create_base_tree()
 
+    def _create_base_tree(self):
+        root = Node({"key": "root", "value": "total"}, {"webreq": 0, "timespent": 0})
+        country_level = [
+            [{"key": "country", "value": "IN"}, {"webreq": 0, "timespent": 0}]
+        ]
+        root.children = [Node(value, metrics) for value, metrics in country_level]
+        device_level = [
+            [{"key": "device", "value": "mobile"}, {"webreq": 0, "timespent": 0}],
+            [{"key": "device", "value": "web"}, {"webreq": 0, "timespent": 0}],
+        ]
+        for country in root.children:
+            children = []
+            for value, metrics in device_level:
+                children.append(Node(value, metrics))
+            country.children = children
+        return root
 
-def show_tree(root):
-    queue = [root] if root else []
-    while queue:
-        next_level = []
-        for node in queue:
-            print(node)
-            if node.children:
-                next_level.extend([child for child in node.children])
-        queue = next_level
-
-
-def insert_nodes(root, dimensions, metrics):
-    queue = [root] if root else []
-    country_not_found_flag = True
-    device_not_found_flag = True
-    while queue:
+    def _insert_nodes(self, root, dimensions, metrics):
+        queue = [root] if root else []
         country_not_found_flag = True
         device_not_found_flag = True
-        next_level = []
-        country_level = []
-        for node in queue:
-            if node.dim_type == "root":
-                country_level = [child for child in node.children]
-                for country in country_level:
-                    if dimensions.get("country") == country.dim_value:
-                        country_not_found_flag = False
-                        country.metrics["webreq"] += metrics.get("webreq")
-                        country.metrics["timespent"] += metrics.get("timespent")
-                        root.metrics["webreq"] += metrics.get("webreq")
-                        root.metrics["timespent"] += metrics.get("timespent")
-                        if country.children:
-                            device_level = [child for child in country.children]
-                            for device in device_level:
-                                if device.dim_value == dimensions.get("device"):
+        while queue:
+            country_not_found_flag = True
+            device_not_found_flag = True
+            next_level = []
+            country_level = []
+            for node in queue:
+                if node.dim_type == "root":
+                    country_level = [child for child in node.children]
+                    for country in country_level:
+                        if dimensions.get("country") == country.dim_value:
+                            country_not_found_flag = False
+                            country.metrics["webreq"] += metrics.get("webreq")
+                            country.metrics["timespent"] += metrics.get("timespent")
+                            root.metrics["webreq"] += metrics.get("webreq")
+                            root.metrics["timespent"] += metrics.get("timespent")
+                            if country.children:
+                                device_level = [child for child in country.children]
+                                for device in device_level:
+                                    if device.dim_value == dimensions.get("device"):
+                                        device_not_found_flag = False
+                                        device.metrics["webreq"] += metrics.get(
+                                            "webreq"
+                                        )
+                                        device.metrics["timespent"] += metrics.get(
+                                            "timespent"
+                                        )
+                                        next_level = []
+                                        queue = next_level
+                                        break
+                                if device_not_found_flag:
                                     device_not_found_flag = False
-                                    device.metrics["webreq"] += metrics.get("webreq")
-                                    device.metrics["timespent"] += metrics.get(
-                                        "timespent"
+                                    country.children.append(
+                                        Node(
+                                            {
+                                                "key": "device",
+                                                "value": dimensions.get("device"),
+                                            },
+                                            {
+                                                "webreq": metrics.get("webreq"),
+                                                "timespent": metrics.get("timespent"),
+                                            },
+                                        )
                                     )
                                     next_level = []
                                     queue = next_level
                                     break
-                            if device_not_found_flag:
-                                device_not_found_flag = False
+                            else:
                                 country.children.append(
                                     Node(
                                         {
@@ -90,77 +100,56 @@ def insert_nodes(root, dimensions, metrics):
                                 next_level = []
                                 queue = next_level
                                 break
-                        else:
-                            country.children.append(
-                                Node(
-                                    {
-                                        "key": "device",
-                                        "value": dimensions.get("device"),
-                                    },
-                                    {
-                                        "webreq": metrics.get("webreq"),
-                                        "timespent": metrics.get("timespent"),
-                                    },
-                                )
+                    if country_not_found_flag:
+                        country_node = Node(
+                            {"key": "country", "value": dimensions.get("country")},
+                            {
+                                "webreq": metrics.get("webreq"),
+                                "timespent": metrics.get("timespent"),
+                            },
+                        )
+                        country_node.children = [
+                            Node(
+                                {"key": "device", "value": dimensions.get("device")},
+                                {
+                                    "webreq": metrics.get("webreq"),
+                                    "timespent": metrics.get("timespent"),
+                                },
                             )
-                            next_level = []
-                            queue = next_level
-                            break
-                if country_not_found_flag:
-                    country_node = Node(
-                        {"key": "country", "value": dimensions.get("country")},
-                        {
-                            "webreq": metrics.get("webreq"),
-                            "timespent": metrics.get("timespent"),
-                        },
-                    )
-                    device_node = Node(
-                        {"key": "device", "value": dimensions.get("device")},
-                        {
-                            "webreq": metrics.get("webreq"),
-                            "timespent": metrics.get("timespent"),
-                        },
-                    )
-                    root.children.append(country_node)
-                    country_node.children.append(device_node)
-                    root.metrics["webreq"] += metrics.get("webreq")
-                    root.metrics["timespent"] += metrics.get("timespent")
-                    country_not_found_flag = False
-                    next_level = []
-                    queue = next_level
-                    break
-        queue = next_level
+                        ]
+                        root.children.append(country_node)
+                        root.metrics["webreq"] += metrics.get("webreq")
+                        root.metrics["timespent"] += metrics.get("timespent")
+                        country_not_found_flag = False
+                        next_level = []
+                        queue = next_level
+                        break
+            queue = next_level
 
+    def insert_data(self, data):
+        dimensions = {dim.get("key"): dim.get("val") for dim in data.get("dim")}
+        metrics = {
+            metrics.get("key"): metrics.get("val") for metrics in data.get("metrics")
+        }
+        self._insert_nodes(self.root, dimensions, metrics)
+        return status.HTTP_200_OK
 
-def insert_data(data, root):
-    dimensions = {dim.get("key"): dim.get("val") for dim in data.get("dim")}
-    metrics = {
-        metrics.get("key"): metrics.get("val") for metrics in data.get("metrics")
-    }
-    insert_nodes(root, dimensions, metrics)
+    def fetch_data(self, key, value):
+        response = None
+        queue = [self.root] if self.root else []
+        while queue:
+            next_level = []
+            for node in queue:
+                if node.dim_value == value:
+                    response = {
+                        "dim": [{"key": key, "val": node.dim_value}],
+                        "metrics": [
+                            {"key": "webreq", "val": node.metrics.get("webreq")},
+                            {"key": "timespent", "val": node.metrics.get("timespent")},
+                        ],
+                    }
+                    return response, status.HTTP_200_OK
+                if node.children:
+                    next_level.extend([child for child in node.children])
+            queue = next_level
 
-
-def main():
-    root = Node({"key": "root", "value": "total"}, {"webreq": 0, "timespent": 0})
-    create_base_tree(root)
-    show_tree(root)
-    data = {
-        "dim": [{"key": "device", "val": "mobile"}, {"key": "country", "val": "IN"}],
-        "metrics": [{"key": "webreq", "val": 70}, {"key": "timespent", "val": 30}],
-    }
-    insert_data(data, root)
-    data = {
-        "dim": [{"key": "device", "val": "mobile"}, {"key": "country", "val": "IN"}],
-        "metrics": [{"key": "webreq", "val": 70}, {"key": "timespent", "val": 30}],
-    }
-    insert_data(data, root)
-    data = {
-        "dim": [{"key": "device", "val": "mobile"}, {"key": "country", "val": "US"}],
-        "metrics": [{"key": "webreq", "val": 70}, {"key": "timespent", "val": 30}],
-    }
-    insert_data(data, root)
-    show_tree(root)
-
-
-if __name__ == "__main__":
-    main()
